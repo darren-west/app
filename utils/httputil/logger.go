2d597b56af9028d0778c3be174220d55f64cff49
Darren West <west.darren@gmail.com>
2018-11-15 15:15:17 +0000

    added some basic http logging middleware

---
+++ utils/httputil/logger.go
@@ -0,0 +1,29 @@
+package httputil
+
+import (
+	"net/http"
+	"net/http/httputil"
+
+	"github.com/google/uuid"
+	"github.com/sirupsen/logrus"
+)
+
+// WithHandlerLogging adds a log message for every request/response handled.
+func WithHandlerLogging(l *logrus.Logger, h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		id := uuid.New().String()
+		decorateLogWithRequest(l, r).WithField("id", id).Debug("incoming request")
+		h.ServeHTTP(w, r)
+		decorateLogWithResponse(l, r).WithField("id", id).Debug("outgoing response")
+	})
+}
+
+func decorateLogWithRequest(l *logrus.Logger, r *http.Request) *logrus.Entry {
+	data, _ := httputil.DumpRequest(r, true)
+	return l.WithField("http request", string(data))
+}
+
+func decorateLogWithResponse(l *logrus.Logger, r *http.Request) *logrus.Entry {
+	data, _ := httputil.DumpResponse(r.Response, true)
+	return l.WithField("http request", string(data))
+}
